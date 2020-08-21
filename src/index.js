fa81788108b998b7b5874a5c92214aa754df8cc3
wascoyur <mytem20007@gmail.com>
2020-08-21 16:16:02 +0500

    11:33

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './styles/main.css'
+import {model} from './model'
+
+model.forEach(block => {
+  if(block.type === 'title'){
+    html = title(block)
+  } else if(block.type ==='text'){
+    html = text(block)
+  } else if(block.type === 'textColumns'){
+
+  }
+})
+function title(block){
+  return`
+    <div class="row">
+      <div class="col-sm">
+        <h1>${block.value}</h1>
+      </div>
+    </div>
+  `
+}
+function text(block) {
+  return`
+    <div class="row">
+      <div class="col-sm">
+        <p>${block.value}</p>
+      </div>
+    </div>
+  `
+}
+function textColumns(block) {
+  const html = block.value.map(item => `<div class="col-sm">${item}</div>`)
+  return `
+    <div class="row">
+      <div class="col-sm">
+        <p>${block.value}</p>
+      </div>
+    </div>
+  `
+}
---
+++ src/model.js
@@ -0,0 +1,9 @@
+export const model = [
+  {type: '', value: 'Test Title', options:{}},
+  { type: '', value: 'Lorem ipsum dolor sit, amet consectetur adipisicing ', options:{}},
+  {type:'', value:[
+    '1 text',
+    '2 text',
+    '3 text'
+  ], options:{}}
+]
