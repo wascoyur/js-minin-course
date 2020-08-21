3bb6ebdbe9405394e124c714ba7a982d91ef6291
wascoyur <mytem20007@gmail.com>
2020-08-21 07:54:10 +0500

    start

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output:{
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'distr')
+  },
+  devServer:{
+    port: 3000
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  }
+}
